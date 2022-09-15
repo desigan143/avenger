e276dcc14e4c9864c81e9a1c82bcfbd4422b9bf4
desigan143 <desigand415@gmail.com>
2022-09-15 19:34:22 +0530

    iron updated code

--- src/desi/Desi.java
+++ src/desi/Desi.java
@@ -8,5 +8,20 @@ public class Desi {
 		System.out.println("employee");
 
 	}
+	private void test() {
+		System.out.println("test");
+
+	}
+	private void test2() {
+		System.out.println("desigan");
+
+	}
+	public static void main(String[] args) {
+		Desi d =new Desi();
+		
+		d.employee();
+		d.test();
+		d.test2();
+	}
  
 }
